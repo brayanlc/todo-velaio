ec58df2edb8e893ba7ee82e6ae71fbdaef884730
brayan <brayanlc82@gmail.com>
2024-09-28 12:13:23 -0500

    Improve routing, styles, and dependencies for Todo app
    Added routes for listing and creating todos in app-routing.module.ts. Introduced uuid dependency and streamlined global styles in styles.scss. Cleaned up the placeholder content from app.component.html.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListTodoComponent } from './modules/todo/list-todo/list-todo.component';
+import { CreateTodoComponent } from './modules/todo/create-todo/create-todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'todo',
+    children: [
+      {
+        path: 'create',
+        component: CreateTodoComponent,
+      },
+      {
+        path: 'list',
+        component: ListTodoComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
